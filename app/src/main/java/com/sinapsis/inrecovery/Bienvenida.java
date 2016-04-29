da07012222f21732c20b4494d906dd8c81472548
FelipeTorr <felipto@hotmail.es>
2016-04-28 23:15:02 -0500

    Primera Codificación
    Código
    Bienvenida.
    Selección.

--- app/src/main/java/com/sinapsis/inrecovery/Bienvenida.java
+++ app/src/main/java/com/sinapsis/inrecovery/Bienvenida.java
@@ -1,13 +1,41 @@
 package com.sinapsis.inrecovery;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
 
 public class Bienvenida extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        //Coloca el contenido del layout en pantalla al crar la activity
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_bienvenida);
+        //Declaración de variables privadas
+        Button nuevo=(Button) findViewById(R.id.nuevo);
+        Button veterano=(Button) findViewById(R.id.veterano);
+        //Evento click al botón nuevo
+        nuevo.setOnClickListener(
+                new Button.OnClickListener() {
+                    public void onClick(View v) {
+                        Intent i=new Intent(Bienvenida.this,Seleccion.class);
+                        startActivity(i);
+                    }
+                }
+        );
+        //Evento  click al botón ya tengo cuenta
+        veterano.setOnClickListener(
+                new Button.OnClickListener() {
+                    public void onClick(View v) {
+                        Intent i=new Intent(Bienvenida.this,MenuHerramientas.class);
+                        startActivity(i);
+                    }
+                }
+        );
     }
+
 }
--- app/src/main/java/com/sinapsis/inrecovery/Seleccion.java
+++ app/src/main/java/com/sinapsis/inrecovery/Seleccion.java
@@ -2,6 +2,10 @@ package com.sinapsis.inrecovery;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import java.util.List;
 
 public class Seleccion extends AppCompatActivity {
 
@@ -9,5 +13,10 @@ public class Seleccion extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_seleccion);
+        Spinner adicciones=(Spinner) findViewById(R.id.adicciones);
+        // Crear adaptador para spinner
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_dropdown_item,getResources().getStringArray(R.array.adicciones));
+        adicciones.setAdapter(dataAdapter);
+
     }
 }
