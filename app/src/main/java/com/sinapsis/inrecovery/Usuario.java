580b22be4132499545a7d5649b39e7db6c3779a5
FelipeTorr <felipto@hotmail.es>
2016-06-09 18:14:40 -0500

    cambios del 9 de junio

--- app/src/main/java/com/sinapsis/inrecovery/Usuario.java
+++ app/src/main/java/com/sinapsis/inrecovery/Usuario.java
@@ -11,14 +11,22 @@ public class Usuario {
     public int puntaje;
     public int dias_sin_adiccion;
     public int dias_usando_app;
-    public void Usuario (String nombre, int edad){
+	public int adiccion_pref;
+	public int[] progreso;
+
+    public Usuario (String nombre, int edad){
         this.edad=edad;
         this.nombre=nombre;
         this.puntaje=0;
         this.dias_sin_adiccion=0;
         this.dias_usando_app=0;
+		this.progreso=new int[4];
+		progreso[0]=0;
+		progreso[1]=0;
+		progreso[2]=0;
+		progreso[3]=0;
     }
-    public void Usuario (String nombre,String contrasena, int edad){
+    public Usuario (String nombre,String contrasena, int edad){
         this.edad=edad;
         this.contrasena=contrasena;
         this.nombre=nombre;
@@ -27,6 +35,10 @@ public class Usuario {
         this.dias_usando_app=0;
     }
 
+	public void setAdiccion_pref(int adiccion) {
+        this.adiccion_pref=adiccion;
+    }
+	
     public void setDias_usando_app(int dias_usando_app) {
         this.dias_usando_app = dias_usando_app;
     }
@@ -38,5 +50,9 @@ public class Usuario {
     public void setPuntaje(int puntaje) {
         this.puntaje = puntaje;
     }
-
+	
+	public void SumarPunto(int categoria) {
+        this.puntaje++;
+		this.progreso[categoria]++;
+    }
 }
